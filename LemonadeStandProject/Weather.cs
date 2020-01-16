9da0eb31eadf65a05d9e8d056978cd667889a601
Andrew Baird <baird.andrew4@gmail.com>
2020-01-16 11:16:22 -0600

    Eliminated conditionlist in Weather Class because weatherConditions was the same list. Put Choose condition and choose temp to be called in RunWeatherSelection method.

--- LemonadeStandProject/Weather.cs
+++ LemonadeStandProject/Weather.cs
@@ -13,26 +13,31 @@ namespace LemonadeStandProject
         public string condition;
         public int temperature;
         private List<string> weatherConditions;
-        private List<string> conditionlist;
         public string predictedForecast;
 
         //Constructor
         public Weather()
         {
-            conditionlist = new List<string>() { "Sunny", "Cloudy", "Rainy", "Overcast", "Windy", "Scattered Showers", "Hazy", "Foggy" };
+            weatherConditions = new List<string>() { "Sunny", "Cloudy", "Rainy", "Overcast", "Windy", "Scattered Showers", "Hazy", "Foggy" };
         }
         //Member Methods (CAN DO)
+        public void RunWeatherSelection()
+        {
+            ChooseCondition();
+            ChooseTemp();
+        }
         public void ChooseCondition()
         {
             Random random = new Random();
-            condition = conditionlist[random.Next((conditionlist.Count + 1))];
+            condition = weatherConditions[random.Next((weatherConditions.Count + 1))];
         }
 
         public void ChooseTemp()
         {
             Random random = new Random();
-            temperature = random.Next(45, 105);
+            temperature = random.Next(40, 115);
         }
+
     }
 }
 
