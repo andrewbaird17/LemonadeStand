7bef69d5b5fe141a423f4e51a2b4acd8f5876563
unknown <ajpbarry@gmail.com>
2020-01-16 09:25:58 -0600

    created Game, Player, Day, and Store class, made variables for game class

---
+++ LemonadeStandProject/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStandProject
+{
+    public class Game
+    {
+        //Member Variables (HAS A)
+        Player player;
+        List<Day>days;
+        int currentDay;
+        //Constructor
+
+        //Member Methods (CAN DO)
+        public void Run()
+        {
+
+        }
+
+        }
+}
--- LemonadeStandProject/Store.cs
+++ LemonadeStandProject/Store.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace LemonadeStandProject
 {
-    class Program
+    class Store
     {
-        static void Main(string[] args)
-        { Game game = new Game;
-            game.Run();
-        }
     }
 }
