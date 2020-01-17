ab668bbda52f8f12072e206389a2fa17a52893b2
Andrew Baird <baird.andrew4@gmail.com>
2020-01-17 07:06:34 -0600

    Made a new blank method for selling lemons in the store class

--- LemonadeStandProject/Store.cs
+++ LemonadeStandProject/Store.cs
@@ -27,40 +27,43 @@ namespace LemonadeStandProject
         {
             MakePurchases();
         }
+        public void SellLemons()
+        {
+
+        }
         public void MakePurchases()
         {
             Console.WriteLine("Welcome to The Store! What do you want to do?\n" +
                                 "1) Lemon\n2) SugarCube\n3) IceCube\n4) Cup\n5)Exit");
             string selection = Console.ReadLine().ToLower();
-            do
+
+            switch (selection)
             {
-                switch (selection)
-                {
-                    case "1":
-                    case "lemon":
-                        break;
-                    case "2":
-                    case "sugarcube":
-                    case "sugar cube":
-                        break;
-                    case "3":
-                    case "icecube":
-                    case "ice cube":
-                        break;
-                    case "4":
-                    case "cup":
-                        break;
-                    case "5":
-                    case "exit":
-                        break;
-                    default:
-                        Console.WriteLine("Please enter a valid selection.");
-                        MakePurchases();
-                        break;
-                }
-            } while (selection != "exit");
+                case "1":
+                case "lemon":
+                    break;
+                case "2":
+                case "sugarcube":
+                case "sugar cube":
+                    break;
+                case "3":
+                case "icecube":
+                case "ice cube":
+                    break;
+                case "4":
+                case "cup":
+                    break;
+                case "5":
+                case "exit":
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid selection.");
+                    MakePurchases();
+                    break;
+
+
+            }
 
         }
-
     }
 }
