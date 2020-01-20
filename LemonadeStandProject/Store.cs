decf7994f0187a47625030f347238542065ee903
unknown <ajpbarry@gmail.com>
2020-01-20 11:29:01 -0600

    fixed lemon error, pushing to partner

--- LemonadeStandProject/Store.cs
+++ LemonadeStandProject/Store.cs
@@ -10,6 +10,7 @@ namespace LemonadeStandProject
     {
         //Member Variables (HAS A)       
         Inventory storeInventory;
+        Player player;
         public int numberOfItems;
         public double saleCost;
         //Constructor
@@ -23,26 +24,59 @@ namespace LemonadeStandProject
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.lemon.purchasePrice * numberOfItems;
+            AddLemonsToInventory(numberOfItems);
             return saleCost;
+        }
+        public void AddLemonsToInventory(int numberOfItems)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                player.inventory.lemons.Add(new Lemon());
+            }
         }
         public double SellSugar()
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.sugarcube.purchasePrice * numberOfItems;
+            AddSugarToInventory(numberOfItems);
             return saleCost;
+        }
+        public void AddSugarToInventory(int numberOfItems)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                player.inventory.sugarCubes.Add(new SugarCube());
+            }
         }
         public double SellIce()
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.icecube.purchasePrice * numberOfItems;
+            AddIceToInventory(numberOfItems);
             return saleCost;
+        }
+        public void AddIceToInventory(int numberOfItems)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                player.inventory.icecubes.Add(new IceCube());
+            }
         }
         public double SellCups()
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.cup.purchasePrice * numberOfItems;
+            AddCupToInventory(numberOfItems);
             return saleCost;
         }
+        public void AddCupToInventory(int numberOfItems)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                player.inventory.cups.Add(new Cup());
+            }
+        }
+      
         public void NumberItemsToPurchase()
         {
             Console.WriteLine("How many items do you want?");
