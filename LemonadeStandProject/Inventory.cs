ba46d98edc5c6e61a0c70162e7b4228f9beaa434
unknown <ajpbarry@gmail.com>
2020-01-21 07:05:38 -0600

    Change

--- LemonadeStandProject/Inventory.cs
+++ LemonadeStandProject/Inventory.cs
@@ -13,8 +13,7 @@ namespace LemonadeStandProject
         public List<IceCube> icecubes;
         public List<Cup> cups;
         public List<SugarCube> sugarCubes;
-        Recipe 
-            recipe;
+        Recipe recipe;
         public Lemon lemon;
         public SugarCube sugarcube;
         public IceCube icecube;
