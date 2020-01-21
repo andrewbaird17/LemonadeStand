d934bb447f02d9ec2c4f32a889a470d7891c911e
Andrew Baird <baird.andrew4@gmail.com>
2020-01-21 08:29:06 -0600

    Made itemPurchase variable in the game class private and fixed a spacing in a console writeline in game class

--- LemonadeStandProject/Game.cs
+++ LemonadeStandProject/Game.cs
@@ -17,7 +17,7 @@ namespace LemonadeStandProject
         int servedCustomers;
         double begindaysMoney;
         double enddaysMoney;
-        public string itemPurchase;
+        string itemPurchase;
 
         //Constructor
         public Game()
@@ -183,7 +183,7 @@ namespace LemonadeStandProject
             {
                 Console.Clear();
                 Console.WriteLine("CARD DECLINED!\n" +
-                    "This costs $" + saleCost + ", you only have $" + player.wallet.Money + "remaining.\n\n");
+                    "This costs $" + saleCost + ", you only have $" + player.wallet.Money + " remaining.\n\n");
                 ContinueShopping();
             }
             else
--- LemonadeStandProject/Store.cs
+++ LemonadeStandProject/Store.cs
@@ -19,62 +19,29 @@ namespace LemonadeStandProject
             storeInventory = new Inventory();
         }
         //Member Methods (CAN DO)
-
         public double SellLemons(Player player)
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.lemon.purchasePrice * numberOfItems;
-            //AddLemonsToInventory(numberOfItems, player);
             return saleCost;
-        }
-        public void AddLemonsToInventory(int numberOfItems, Player player)
-        {
-            for (int i = 0; i < numberOfItems; i++)
-            {
-                player.inventory.lemons.Add(new Lemon());
-            }
         }
         public double SellSugar(Player player)
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.sugarcube.purchasePrice * numberOfItems;
-            //AddSugarToInventory(numberOfItems, player);
             return saleCost;
-        }
-        public void AddSugarToInventory(int numberOfItems, Player player)
-        {
-            for (int i = 0; i < numberOfItems; i++)
-            {
-                player.inventory.sugarCubes.Add(new SugarCube());
-            }
         }
         public double SellIce(Player player)
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.icecube.purchasePrice * numberOfItems;
-            //AddIceToInventory(numberOfItems, player);
             return saleCost;
-        }
-        public void AddIceToInventory(int numberOfItems, Player player)
-        {
-            for (int i = 0; i < numberOfItems; i++)
-            {
-                player.inventory.icecubes.Add(new IceCube());
-            }
         }
         public double SellCups(Player player)
         {
             NumberItemsToPurchase();
             saleCost = storeInventory.cup.purchasePrice * numberOfItems;
-            //AddCupToInventory(numberOfItems, player);
             return saleCost;
-        }
-        public void AddCupToInventory(int numberOfItems, Player player)
-        {
-            for (int i = 0; i < numberOfItems; i++)
-            {
-                player.inventory.cups.Add(new Cup());
-            }
         }
         public void AddItemsToInventory(int numberOfItems, Player player, string itemPurchase)
         {
@@ -101,8 +68,7 @@ namespace LemonadeStandProject
                     break;
                 }
             }
-        }
-      
+        }    
         public void NumberItemsToPurchase()
         {
             Console.WriteLine("How many items do you want?");
